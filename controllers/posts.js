09866b98f4ef60a35e3f23727fa32a78a55ab367
Jorge Paul <jserranopaul@gmail.com>
2024-08-05 19:50:15 +0200

    Authorization

--- controllers/posts.js
+++ controllers/posts.js
@@ -27,20 +27,29 @@ export const getSinglePost = asyncHandler(async (req, res, next) => {
 export const updatePost = asyncHandler(async (req, res, next) => {
   const {
     body,
-    params: { id }
+    params: { id },
+    userId
   } = req;
   if (!isValidObjectId(id)) throw new ErrorResponse('Invalid id', 400);
-  const updatedPost = await Post.findByIdAndUpdate(id, body, { new: true }).populate('author');
-  if (!updatedPost) throw new ErrorResponse(`Post with id of ${id} doesn't exist`, 404);
-  res.json(updatedPost);
+  const post = await Post.findById(id).populate('author');
+  if (!post) throw new ErrorResponse(`Post with id of ${id} doesn't exist`, 404);
+  if (post.author._id.toString() !== userId)
+    throw new ErrorResponse('Not authorized to update this post', 403);
+  post.set(body);
+  await post.save();
+  res.json(post);
 });
 
 export const deletePost = asyncHandler(async (req, res, next) => {
   const {
-    params: { id }
+    params: { id },
+    userId
   } = req;
   if (!isValidObjectId(id)) throw new ErrorResponse('Invalid id', 400);
-  const deletedPost = await Post.findByIdAndDelete(id).populate('author');
-  if (!deletedPost) throw new Error(`Post with id of ${id} doesn't exist`);
+  const post = await Post.findById(id).populate('author');
+  if (!post) throw new ErrorResponse(`Post with id of ${id} doesn't exist`, 404);
+  if (post.author._id.toString() !== userId)
+    throw new ErrorResponse('Not authorized to delete this post', 403);
+  await Post.findByIdAndDelete(id);
   res.json({ success: `Post with id of ${id} was deleted` });
 });
